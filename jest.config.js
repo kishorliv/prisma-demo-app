bf0c25eca1e9c4eb6a8ef4a351a6d7d5e4e1b929
Kishor Neupane <kishorliv@gmail.com>
2022-09-18 14:21:16 +1000

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    testEnvironment: "node",
+    restoreMocks: true,
+    collectCoverageFrom: ["**/*.(t|j)s"],
+    coverageDirectory: "../coverage",
+    transform: {
+      "^.+\\.ts$": "ts-jest",
+    },
+    moduleFileExtensions: ["ts", "js", "json", "node"],
+  };
+  
