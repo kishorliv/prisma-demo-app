c3e51a497ef36e76201baf2aaae7bf9bf2931ac9
Kishor Neupane <kishorliv@gmail.com>
2022-09-22 20:40:45 +1000

    feat: Use prisma to resolve the query

--- packages/api/graphql/schema/link.ts
+++ packages/api/graphql/schema/link.ts
@@ -1,4 +1,5 @@
 import { gql } from 'apollo-server';
+import prisma from '../../util/db';
 
 export const typeDefs = gql`
   type Link {
@@ -18,17 +19,8 @@ export const typeDefs = gql`
 
 export const resolvers = {
   Query: {
-    links: () => {
-      return [
-        {
-          category: 'Open Source',
-          description: 'Fullstack React framework',
-          id: '8a9020b2-363b-4a4f-ad26-d6d55b51bqes',
-          imageUrl: 'https://nextjs.org/static/twitter-cards/home.jpg',
-          title: 'Next.js',
-          url: 'https://nextjs.org',
-        },
-      ];
+    links: async () => {
+      return prisma.link.findMany();
     },
   },
 };
